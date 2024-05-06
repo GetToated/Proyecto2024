64a3e0e555b987a288a72ac9ee407eacdba28ac1
Lucas <lucasfpinformatica@gmail.com>
2024-05-06 16:33:57 +0200

    prueba

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,7 @@ package Main;
 import Proyecto2024.Ahorcado.AhorcadoPrincipal;
 public class Main {
     public static void main(String[] args) {
-        AhorcadoPrincipal partida = new AhorcadoPrincipal(10, 60);
+        AhorcadoPrincipal partida = new AhorcadoPrincipal(10, 6);
         partida.comienzoDelJuego();
 
     }
